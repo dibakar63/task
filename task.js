f1f1c5b529f90c661b5ca72c9d17a003690dd3f4
Dibakar Dey <110836157+dibakar63@users.noreply.github.com>
2023-06-12 11:23:19 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,100 @@
+//task -1
+//1.
+//var x;
+//var y;
+//var z;
+//var a;
+//console.log(x)
+//console.log(y)
+//console.log(z)
+//console.log(a)
+
+//2.
+// var myvar=1;
+// console.log(myvar);
+
+//3.
+// var firstName="Dibakar";
+// var lastName="Dey";
+// var maritialStatus="No";
+// var Country="India";
+// var age="25";
+
+//4.
+// var myBio={
+//     firstName:"Dibakar",
+//     lastName:"Dey",
+//     maritialStatus:"No",
+//     country:"India",
+//     age:"25"
+// }
+
+//5.
+// var x=typeof(String)
+// var y=typeof(Boolean)
+// var z=typeof(undefined)
+// var a=typeof(null)
+// console.log(x,y)
+
+//6.
+// var x="123";
+// var z=parseInt(x);
+// console.log(typeof(z))
+//7.
+// let a=2;
+// let b=3;
+// if(a===b){
+//     return true
+// }else{
+//     return false
+// }
+
+
+
+//task2.
+// function square(n){
+//     console.log(n*n);
+// }
+// square(5)
+
+// function swap(a,b){
+//     console.log(b,a)
+// }
+// swap(5,2)
+// function add3(n){
+//     console.log(n+3);
+// }
+// add3(5)
+// function c2f(a){
+//     let f=(a*9)/5 +32;
+//     console.log(f)
+// }
+//  c2f(32);
+
+//  function testScore(a,b,c,d,e){
+//     let x=(a+b+c+d+e)/5;
+//     console.log(x)
+//  }
+//  testScore(20,23,24,23,25);
+
+//  function powerOfanyNo(a,b){
+//    let x=Math.pow(a,b);
+//     console.log(x)
+//  }
+//  powerOfanyNo(5,4);
+ 
+//  function discount(a,b){
+//     let x=(a-b)*100/a
+     
+//      console.log(x)
+//  }
+//  discount(100,80)
+ //task 3.
+//  for (i = 1; i <= 5; i = i + 1) {
+//     let str = "";
+//     for (j = 0; j < i; j = j + 1) {
+//       str += "*";
+//     }
+//     console.log(str);
+//   }
+  
